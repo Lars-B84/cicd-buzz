7f87c424b953503c42a30f0816f273fcf13815e9
Lars <larsboer@gmail.com>
2019-03-30 22:39:16 +0100

    laatse

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
-docker push $DOCKER_PATH
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG
