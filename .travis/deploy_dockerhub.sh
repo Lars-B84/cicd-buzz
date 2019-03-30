1c6027b307b96b5f42686a6575e6a380fe2f02f4
Lars <larsboer@gmail.com>
2019-03-30 22:53:58 +0100

    needs

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t larsboer/cicd-buzz:$TAG .
-docker push larsboer/cicd-buzz
+docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
+docker push $DOCKER_PATH
